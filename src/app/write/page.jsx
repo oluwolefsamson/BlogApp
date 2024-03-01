57b603fb82137989ffee7ebbd1071a169e606768
Oluwole Samson <oluwolefsamson44@gmail.com>
2024-03-01 13:29:08 +0100

    first commit

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -1,54 +1,48 @@
 "use client"
 
-import { useState, useEffect } from 'react';
-import styles from './writePage.module.css';
-import Image from 'next/image';
+import { useState } from 'react'
+import styles from './writePage.module.css'
+import Image from 'next/image'
 import "react-quill/dist/quill.bubble.css";
 import ReactQuill from 'react-quill';
 
 const WritePage = () => {
+
     const [open, setOpen] = useState(false);
-    const [value, setValue] = useState('');
-
-    useEffect(() => {
-        // Check if document is defined (client-side environment)
-        if (typeof document !== 'undefined') {
-            // Accessing document here is safe
-            // You can perform any client-side operations that involve the DOM
-        }
-    }, []);
-
-    return (
-        <div className={styles.container}>
-            <input type="text" placeholder="Title" className={styles.input}/>
-            <div className={styles.editor}>
-                <button className={styles.button} onClick={() => setOpen(!open)}>
-                    <Image src="/plus.png" alt="" width={16} height={16}/>
-                </button>
-                {open && (
-                    <div className={styles.add}>
-                        <button className={styles.addButton}>
-                            <Image src="/image.png" alt="" width={16} height={16}/>
-                        </button>
-                        <button className={styles.addButton}>
-                            <Image src="/external.png" alt="" width={16} height={16}/>
-                        </button>
-                        <button className={styles.addButton}>
-                            <Image src="/video.png" alt="" width={16} height={16}/>
-                        </button>
-                    </div>
-                )}
+    const [value, setValue] = useState(false);
+  return (
+    <div className={styles.container}>
+        <input type="text" placeholder="Title" className={styles.input}/>
+        <div className={styles.editor}>
+            <button className={styles.button} onClick={() => setOpen(!open)}>
+                <Image src="/plus.png" alt="" width={16} height={16}/>
+            </button>
+            {open && (
+                <div className={styles.add}>
+                    <button className={styles.addButton}>
+                        <Image src="/image.png" alt="" width={16} height={16}/>
+                    </button>
+                    <button className={styles.addButton}>
+                        <Image src="/external.png" alt="" width={16} height={16}/>
+                    </button>
+                    <button className={styles.addButton}>
+                        <Image src="/video.png" alt="" width={16} height={16}/>
+                    </button>
+                </div>
+            )}
+            
                 <ReactQuill
-                    className={styles.textArea}
-                    theme='bubble'
-                    value={value} 
-                    onChange={setValue} 
-                    placeholder='Tell us your story.....'
+                className={styles.textArea}
+                theme='bubble'
+                value={value} 
+                onChange={setValue} 
+                placeholder='Tell us your story.....'
                 />
-            </div>
-            <button className={styles.publish}>Publish</button>
+            
         </div>
-    );
+        <button className={styles.publish}>Publish</button>
+    </div>
+  )
 }
 
-export default WritePage;
+export default WritePage
